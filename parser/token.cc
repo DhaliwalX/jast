82d84b257c8c8a9030cdb097471619e39026a1b9
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-08-04 19:35:59 +0530

    Made static members global

--- parser/token.cc
+++ parser/token.cc
@@ -1,11 +1,11 @@
 #include "parser/token.h"
-const char *Token::tokens_[NUM_TOKENS] = {
+const char *tokens_[NUM_TOKENS] = {
 #define STR(t, s, p) s,
         TOKEN_LIST(STR)
 #undef STR
 };
 
-const int Token::precedence_[NUM_TOKENS] = {
+const int precedence_[NUM_TOKENS] = {
 #define PRECEDENCE(t, s, p) p,
         TOKEN_LIST(PRECEDENCE)
 #undef PRECEDENCE
--- parser/token.h
+++ parser/token.h
@@ -4,14 +4,15 @@
 #include "parser/tokens.h"
 #include <string>
 
+extern const char *tokens_[NUM_TOKENS];
+extern const int precedence_[NUM_TOKENS];
+
 class Token {
 public:
     static int precedance(int tok) { return precedence_[tok]; }
 
     static const char *str(int tok) { return tokens_[tok]; }
 private:
-    static const char *tokens_[NUM_TOKENS];
-    static const int precedence_[NUM_TOKENS];
 };
 
 #endif

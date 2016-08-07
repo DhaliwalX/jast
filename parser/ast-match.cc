03d815527abd71d5d223220751f2a30340a3c78a
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-08-07 22:00:29 +0530

    Add AST match

---
+++ parser/ast-match.cc
@@ -0,0 +1,25 @@
+#ifndef AST_MATCH_H_
+#define AST_MATCH_H_
+
+#include "parser/expression.h"
+#include "parser/statement.h"
+
+namespace grok {
+namespace parser {
+
+bool FastASTMatcher::match(Expression *a, Expression *b)
+{
+    // TODO := write code here...
+    return false;
+}
+
+bool LazyASTMatcher::match(Expression *a, Expression *b)
+{
+    // TODO := write code here...
+    return false;
+}
+
+}
+}
+
+#endif

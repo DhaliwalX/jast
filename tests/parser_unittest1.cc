c8c0a2dec9ca39beef371faa80428f3f2e2a559b
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-08-07 20:05:36 +0530

    Add more tests

--- tests/parser_unittest1.cc
+++ tests/parser_unittest1.cc
@@ -68,16 +68,16 @@ namespace {
 #define GET_STATEMENT(ast, nr)  \
     *(ast->AsBlockStatement()->statements()->begin() + nr)
 
-#define LOG_TEST_CASE(class, name, case, count)   \
+#define LOG_TEST_CASE(class, name, c, count)   \
     do { std::ostringstream os;  \
     std::string intro = "case #" + std::to_string(count);   \
-    os << INTRO(class, name) << ": '" << case << "'";   \
+    os << INTRO(class, name) << ": '" << c << "'";   \
     LOG(intro, os.str()); } while (0) 
 
-#define LOG_TEST_CASE_PASSED(class, name, case, count)  \
+#define LOG_TEST_CASE_PASSED(class, name, c, count)  \
     do { std::ostringstream os;  \
     std::string intro = "case #" + std::to_string(count++);   \
-    os << INTRO(class, name) << ": '" << case << "' passed";   \
+    os << INTRO(class, name) << ": '" << c << "' passed";   \
     LOG(intro, os.str());   } while(0)
 
 #define TEST_INTEGRAL_INTERNAL(class, name, nr, count)   \
@@ -85,9 +85,9 @@ do {   \
     LOG_TEST_CASE(class, name, nr, count);  \
     input = #nr;    \
     input.push_back(';');   \
-    Expression *ast = ParseProgram(parser, input);  \
-    ASSERT_EQUAL(ast->type(), ASTNodeType::kBlockStatement);    \
-    ast = GET_STATEMENT(ast, 0);    \
+    std::unique_ptr<Expression> a{ ParseProgram(parser, input) };  \
+    ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);    \
+    Expression *ast = GET_STATEMENT(a, 0);    \
     ASSERT_EQUAL(ast->type(), ASTNodeType::kIntegralLiteral);   \
     ASSERT_EQUAL(ast->AsIntegralLiteral()->value(), nr);    \
     LOG_TEST_CASE_PASSED(class, name, nr, count);   \
@@ -122,25 +122,25 @@ TEST(Syntax, CheckIntegralLiteral) {
     TEST_TEARDOWN(Syntax, CheckIntegralLiteral);
 }
 
-#define TEST_STRING_INTERNAL(class, name, case, count)  \
+#define TEST_STRING_INTERNAL(class, name, c, count)  \
 do {   \
-    LOG_TEST_CASE(class, name, case, count);  \
-    input = case;    \
+    LOG_TEST_CASE(class, name, c, count);  \
+    input = c;    \
     input.push_back(';');   \
-    Expression *ast = ParseProgram(parser, input);  \
-    ASSERT_EQUAL(ast->type(), ASTNodeType::kBlockStatement);    \
-    ast = GET_STATEMENT(ast, 0);    \
+    std::unique_ptr<Expression> a{ ParseProgram(parser, input) };  \
+    ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);    \
+    Expression *ast = GET_STATEMENT(a, 0);    \
     ASSERT_EQUAL(ast->type(), ASTNodeType::kStringLiteral);   \
-    ASSERT_EQUAL(ast->AsStringLiteral()->string(), case);    \
-    LOG_TEST_CASE_PASSED(class, name, case, count);   \
+    ASSERT_EQUAL(ast->AsStringLiteral()->string(), c);    \
+    LOG_TEST_CASE_PASSED(class, name, c, count);   \
 } while (0)
 
-#define TEST_STRING(case)   \
-    TEST_STRING_INTERNAL(Syntax, CheckStringLiteral, case, count)
+#define TEST_STRING(c)   \
+    TEST_STRING_INTERNAL(Syntax, CheckStringLiteral, c, count)
 
 TEST(Syntax, CheckStringLiteral) {
-    DECLARE_TEST(Syntax, CheckIntegralLiteral);
-    TEST_SETUP(Syntax, CheckIntegralLiteral);
+    DECLARE_TEST(Syntax, CheckStringLiteral);
+    TEST_SETUP(Syntax, CheckStringLiteral);
 
     {
         std::string input;
@@ -157,7 +157,130 @@ TEST(Syntax, CheckStringLiteral) {
 }
 
 #undef TEST_STRING
-#undef TEST_STRING_INTERNAL
+
+TEST(Syntax, CheckNullLiteral) {
+    DECLARE_TEST(Syntax, CheckNullLiteral);
+    TEST_SETUP(Syntax, CheckNullLiteral);
+
+    {
+        std::string input;
+        int count = 1;
+
+        LOG_TEST_CASE(Syntax, CheckNullLiteral, "null", count);
+        input = "null";
+        input.push_back(';');
+        std::unique_ptr<Expression> a{ ParseProgram(parser, input) };
+        ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);
+        Expression *ast = GET_STATEMENT(a, 0);
+        ASSERT_EQUAL(ast->type(), ASTNodeType::kNullLiteral);
+        LOG_TEST_CASE_PASSED(Syntax, CheckNullLiteral, input, count);        
+    }
+
+    TEST_TEARDOWN(Syntax, CheckNullLiteral);
+}
+TEST(Syntax, CheckUndefinedLiteral) {
+    DECLARE_TEST(Syntax, CheckUndefinedLiteral);
+    TEST_SETUP(Syntax, CheckUndefinedLiteral);
+
+    {
+        std::string input;
+        int count = 1;
+
+        LOG_TEST_CASE(Syntax, CheckUndefinedLiteral, "undefined", count);
+        input = "undefined";
+        input.push_back(';');
+        std::unique_ptr<Expression> a{ ParseProgram(parser, input) };
+        ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);
+        Expression *ast = GET_STATEMENT(a, 0);
+        ASSERT_EQUAL(ast->type(), ASTNodeType::kIdentifier);
+        LOG_TEST_CASE_PASSED(Syntax, CheckUndefinedLiteral, input, count);
+    }
+
+    TEST_TEARDOWN(Syntax, CheckUndefinedLiteral);
+}
+
+#define TEST_BOOL_INTERNAL(class, name, c, count)  \
+do {   \
+    LOG_TEST_CASE(class, name, c, count);  \
+    input = #c;    \
+    input.push_back(';');   \
+    std::unique_ptr<Expression> a{ ParseProgram(parser, input) };  \
+    ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);    \
+    Expression *ast = GET_STATEMENT(a, 0);    \
+    ASSERT_EQUAL(ast->type(), ASTNodeType::kBooleanLiteral);   \
+    ASSERT_EQUAL(ast->AsBooleanLiteral()->pred(), c);    \
+    LOG_TEST_CASE_PASSED(class, name, c, count);   \
+} while (0)
+
+#define TEST_BOOL(c)   \
+    TEST_BOOL_INTERNAL(Syntax, CheckBooleanLiteral, c, count)
+
+TEST(Syntax, CheckBooleanLiteral) {
+    DECLARE_TEST(Syntax, CheckStringLiteral);
+    TEST_SETUP(Syntax, CheckStringLiteral);
+
+    {
+        std::string input;
+        int count = 1;
+
+        TEST_BOOL(false);
+        TEST_BOOL(true);
+    }
+
+    TEST_TEARDOWN(Syntax, CheckStringLiteral);    
+}
+
+TEST(Syntax, CheckThisHolder) {
+    DECLARE_TEST(Syntax, CheckThisHolder);
+    TEST_SETUP(Syntax, CheckThisHolder);
+
+    {
+        std::string input;
+        int count = 1;
+
+        LOG_TEST_CASE(Syntax, CheckThisHolder, "this", count);
+        input = "this";
+        input.push_back(';');
+        std::unique_ptr<Expression> a{ ParseProgram(parser, input) };
+        ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);
+        Expression *ast = GET_STATEMENT(a, 0);
+        ASSERT_EQUAL(ast->type(), ASTNodeType::kThisHolder);
+        LOG_TEST_CASE_PASSED(Syntax, CheckThisHolder, input, count);
+    }
+
+    TEST_TEARDOWN(Syntax, CheckUndefinedLiteral);
+}
+
+#define TEST_IDENTIFIER(id) \
+    do {   \
+        LOG_TEST_CASE(Syntax, CheckIdentifier, id, count);  \
+        input = id; \
+        input.push_back(';');   \
+        std::unique_ptr<Expression> a{ ParseProgram(parser, input) };   \
+        ASSERT_EQUAL(a->type(), ASTNodeType::kBlockStatement);  \
+        Expression *ast = GET_STATEMENT(a, 0);  \
+        ASSERT_EQUAL(ast->type(), ASTNodeType::kIdentifier);    \
+        ASSERT_EQUAL(ast->AsIdentifier()->GetName(), id);   \
+        LOG_TEST_CASE_PASSED(Syntax, CheckIdentifier, id, count);\
+    } while (0)
+
+TEST(Syntax, CheckIdentifier) {
+    DECLARE_TEST(Syntax, CheckIdentifier);
+    TEST_SETUP(Syntax, CheckIdentifier);
+
+    {
+        std::string input;
+        int count = 1;
+
+        TEST_IDENTIFIER("prince");
+        TEST_IDENTIFIER("_dhaliwal");
+        TEST_IDENTIFIER("_0");
+        TEST_IDENTIFIER("_");
+        TEST_IDENTIFIER("_prince_1924023_dhaliwal");
+    }
+
+    TEST_TEARDOWN(Syntax, CheckUndefinedLiteral);
+}
 
 }
 
@@ -165,4 +288,10 @@ int main()
 {
     CheckIntegralLiteral();
     CheckStringLiteral();
+    CheckNullLiteral();
+    CheckUndefinedLiteral();
+    CheckBooleanLiteral();
+    CheckThisHolder();
+    CheckIdentifier();
+    return 0;
 }

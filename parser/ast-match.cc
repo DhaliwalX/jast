29a264303cf58d8d6f6fdadf547050391f70a9d1
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-08-09 22:36:37 +0530

    Add MatchArrayLiteral

--- parser/ast-match.cc
+++ parser/ast-match.cc
@@ -1,15 +1,51 @@
-#ifndef AST_MATCH_H_
-#define AST_MATCH_H_
-
 #include "parser/expression.h"
 #include "parser/statement.h"
 
 namespace grok {
 namespace parser {
 
+bool MatchArrayLiteral(ArrayLiteral *a, ArrayLiteral *b)
+{
+        auto &a_exprs = a->exprs();
+        auto &b_exprs = b->exprs();
+
+        if (a_exprs.size() != b_exprs.size())
+            return false;
+
+        for (int i = 0; i < a_exprs.size()) {
+            if (!FastASTMatcher::match(a_exprs[i].get(), b_exprs[i].get()))
+                return false;
+        }
+
+        return true;
+}
+
+bool MatchObjectLiteral(ObjectLiteral *a, ObjectLiteral *b)
+{
+    
+}
+
 bool FastASTMatcher::match(Expression *a, Expression *b)
 {
     // TODO := write code here...
+
+    if (a->type() != b->type())
+        return false;
+    switch (a->type()) {
+    case ASTNodeType:kNullLiteral:
+    case ASTNodeType::kUndefinedLiteral:
+    case ASTNodeType::kThisHolder:
+        return true;
+
+    case ASTNodeType::kIntegralLiteral:
+        return a->AsIntegralLiteral()->value() == b->AsIntegralLiteral()->value();
+
+    case ASTNodeType::kStringLiteral:
+        return a->AsStringLiteral()->value() == b->AsStringLiteral()->value();
+
+    case ASTNodeType::kArrayLiteral:
+        return MatchArrayLiteral(a->AsArrayLiteral(), b->AsArrayLiteral());
+    }
     return false;
 }
 
@@ -21,5 +57,3 @@ bool LazyASTMatcher::match(Expression *a, Expression *b)
 
 }
 }
-
-#endif
